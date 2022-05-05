638fff3c44e2fc1e082048b881c0146ae278e361
mibanouguep <rosny.mibanouguep@student.uclouvain.be>
2022-05-05 12:26:29 +0200

    n1

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@ int main(void)
         char *meshName = "../data/PacificFine.txt";
         char *resultBaseName = "../output/tsunamiFine";
         
-        tsunamiCompute(0.1,400,100,meshName,resultBaseName);
-        tsunamiAnimate(0.1,400,100,meshName,resultBaseName);
+        tsunamiCompute(2.,10000,100,meshName,resultBaseName);
+        tsunamiAnimate(2.,10000,100,meshName,resultBaseName);
          
         exit(0);     
 }
